29d9021cad5c9833df381d188db326b567b6b348
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-09-08 21:43:23 +0300

    coloPicker to localStorage

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import colors from "../../assets/colors.json"
 import clsx from "clsx"
 const ColorPicker = () => {
-  const [color, setColor] = useState(colors[0].color)
+  const [color, setColor] = useState(() => {
+    const savedColor = window.localStorage.getItem("colorPicker")
+    if (savedColor !== null) {
+      return JSON.parse(savedColor)
+    }
+    return colors[0].color
+  })
+
+  useEffect(() => {
+    window.localStorage.setItem("colorPicker", JSON.stringify(color))
+  }, [color])
 
   return (
-    <section style={{backgroundColor: color}} className='flex w-full h-full items-center justify-center transition-all '>
+    <section
+      style={{ backgroundColor: color }}
+      className='flex fixed w-full h-full items-center justify-center transition-all '
+    >
       <div className='flex items-center justify-center flex-col gap-2'>
-        <h2 className="font-bold">Current color: <span>{color}</span></h2>
+        <h2 className='font-bold'>
+          Current color: <span>{color}</span>
+        </h2>
         <ul className='flex gap-2 flex-wrap items-center justify-center'>
           {colors.map((item) => (
             <li
-              key={item.id} style={{backgroundColor: item.color}}
+              key={item.id}
+              style={{ backgroundColor: item.color }}
               className={clsx(`border-2 rounded-sm p-1 bg-[${color}]`)}
               onClick={() => setColor(item.color)}
             >
