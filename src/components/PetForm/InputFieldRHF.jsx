6aae9baa1919fb47579d559da98270f763f429e2
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-09-30 14:06:36 +0300

    forms done

--- src/components/PetForm/InputFieldRHF.jsx
+++ src/components/PetForm/InputFieldRHF.jsx
@@ -1,8 +1,9 @@
-export const InputFieldRHF = ({ name, placeholder, register }) => {
+export const InputFieldRHF = ({ name, placeholder, register, errors }) => {
   return (
-    <label>
+    <label className="relative">
       <span>{name}</span>
-      <input {...register(name)} type='text' placeholder={placeholder} />
+      <input {...register(name)} type='text' placeholder={placeholder} className="py-1 px-2 outline-none" />
+      {errors[name] && <span className="absolute bottom-[-10px] left-1 text-[8px] text-red-300">{errors[name].message}</span>}
     </label>
   )
 }
--- src/components/PetForm/PetFormRHF.jsx
+++ src/components/PetForm/PetFormRHF.jsx
@@ -1,17 +1,25 @@
 import { useForm } from "react-hook-form"
 import { InputFieldRHF } from "./InputFieldRHF"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { petFormRHFSchema } from "../../helpers/validationSchemas/validationSchemas"
+import { Button } from "../Button"
 
 export const PetFormRHF = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
-  const onSubmit = data=>console.log(data);
-  
+  } = useForm({
+    resolver: yupResolver(petFormRHFSchema),
+  })
+  const onSubmit = (data) => console.log(data)
+
   return (
     <div className='grid place-content-center h-[100vh]'>
-      <form className='w-[350px]' onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className='w-[350px] flex flex-col gap-2'
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <InputFieldRHF
           register={register}
           name='name'
@@ -23,7 +31,7 @@ export const PetFormRHF = () => {
           name='owner'
           placeholder='Type owner'
           errors={errors}
-        />{" "}
+        />
         <InputFieldRHF
           register={register}
           name='email'
@@ -42,16 +50,22 @@ export const PetFormRHF = () => {
           placeholder='Type age'
           errors={errors}
         />
-        <div>
+        <div className='relative mb-3'>
           <label>
-            <input type='radio' name='gender' value='Male' />
+            <input {...register('gender')} type='radio' name='gender' value='Male' />
             Male
           </label>
           <label>
-            <input type='radio' name='gender' value='Female' />
+            <input {...register('gender')} type='radio' name='gender' value='Female' />
             Female
           </label>
+          {errors.gender && (
+            <span className='absolute bottom-[-10px] left-1 text-[8px] text-red-300'>
+              {errors.gender.message}
+            </span>
+          )}
         </div>
+        <Button type='submit'>Send</Button>
       </form>
     </div>
   )
