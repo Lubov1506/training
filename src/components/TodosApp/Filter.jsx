bd06207457cc9d25a7124acb5620b57f6005c72c
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-10-19 21:05:32 +0300

    need fix

--- src/components/TodosApp/Filter.jsx
+++ src/components/TodosApp/Filter.jsx
@@ -1,16 +1,50 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { selectTodosFilter } from "../../redux/todosFilter/selectors"
+import clsx from "clsx"
+import { changeFilter } from "../../redux/todosFilter/actions"
 
 export const Filter = () => {
   const filter = useSelector(selectTodosFilter)
-  console.log(filter);
-  
+  console.log(filter)
+  const dispatch = useDispatch()
   return (
     <div className='flex gap-2 justify-center items-center'>
-      <button className='btn'>All</button>
-      <button className='btn'>Active</button>
-      <button className='btn'>Completed</button>
-      <button className='btn'>Liked</button>
+      <button
+        onClick={() => dispatch(changeFilter("all"))}
+        className={clsx(
+          "btn",
+          filter === "all" ? "btn-success" : "btn:primary"
+        )}
+      >
+        All
+      </button>
+      <button
+        onClick={() => dispatch(changeFilter("active"))}
+        className={clsx(
+          "btn",
+          filter === "active" ? "btn-success" : "btn:primary"
+        )}
+      >
+        Active
+      </button>
+      <button
+        onClick={() => dispatch(changeFilter("completed"))}
+        className={clsx(
+          "btn",
+          filter === "completed" ? "btn-success" : "btn:primary"
+        )}
+      >
+        Completed
+      </button>
+      <button
+        onClick={() => dispatch(changeFilter("liked"))}
+        className={clsx(
+          "btn",
+          filter === "liked" ? "btn-success" : "btn:primary"
+        )}
+      >
+        Liked
+      </button>
     </div>
   )
 }
