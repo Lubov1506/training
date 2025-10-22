6b8b379a4374cccad493c13c37852094a8b30baa
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-10-22 15:56:51 +0300

    redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -11,23 +11,26 @@ import { ToastContainer } from "react-toastify"
 import { UsersProvider } from "./store/UsersProvider.jsx"
 import { AuthProvider } from "./store/AuthProvider.jsx"
 import { Provider } from "react-redux"
-import { store } from "./redux/store.js"
+import { store, persistor } from "./redux/store.js"
+import { PersistGate } from "redux-persist/integration/react"
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <AuthProvider>
-        <UsersProvider>
-          <GalleryProvider>
-            <TodosProvider>
-              <UserProvider>
-                <App />
-              </UserProvider>
-            </TodosProvider>
-          </GalleryProvider>
-        </UsersProvider>
-      </AuthProvider>
-      <ToastContainer autoClose={1500} />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <AuthProvider>
+          <UsersProvider>
+            <GalleryProvider>
+              <TodosProvider>
+                <UserProvider>
+                  <App />
+                </UserProvider>
+              </TodosProvider>
+            </GalleryProvider>
+          </UsersProvider>
+        </AuthProvider>
+        <ToastContainer autoClose={1500} />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,37 @@
 import { counterReducer } from "./counter/slice"
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import { todoReducer } from "./todoList/slice"
 import { configureStore } from "@reduxjs/toolkit"
 import { todosFilterReducer } from "./todosFilter/slice"
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+}
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    todos: todoReducer,
+    todos: persistReducer(persistConfig, todoReducer),
     todosFilter: todosFilterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: import.meta.env.MODE !== "production",
 })
+export const persistor = persistStore(store)
