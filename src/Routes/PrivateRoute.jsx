2cf54914eea362f10777f00354d29b49f60556ef
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2025-10-28 09:20:48 +0200

    fix

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux"
 import { selectIsLoggedIn } from "../redux/tracker/authSlice"
 
 export const PrivateRoute = ({ children }) => {
-  const  isLoggedIn  = useSelector(selectIsLoggedIn)
+  const isLoggedIn = useSelector(selectIsLoggedIn)
   const location = useLocation()
 
   return isLoggedIn ? children : <Navigate to='/login' state={location} />
